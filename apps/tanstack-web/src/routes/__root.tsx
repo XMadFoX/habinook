c204104a8dd8f69a08f20e9b4ea2d1361324a330
MadFox <madfox@laserflare.net>
2025-04-25 02:44:11 +0400

    feat: init tanstack start

--- apps/tanstack-web/src/routes/__root.tsx
+++ apps/tanstack-web/src/routes/__root.tsx
@@ -9,7 +9,7 @@ export const Route = createRootRoute({
 function RootComponent() {
 	return (
 		<>
-			<div className="p-2 flex gap-2 text-lg ">
+			<div className="p-2 flex gap-2 text-lg">
 				<Link
 					to="/"
 					activeProps={{
