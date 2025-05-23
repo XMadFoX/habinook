f23cace5cdff981d2710c46168014fc6eaa95a68
MadFox <madfox@laserflare.net>
2025-05-24 03:34:40 +0400

    feat(tss-web): update tanstack setup

--- apps/tanstack-web/app/routes/index.tsx
+++ apps/tanstack-web/app/routes/index.tsx
@@ -8,7 +8,8 @@ export const Route = createFileRoute("/")({
 function HomeComponent() {
 	return (
 		<div className="p-2">
-			<h3>Welcome Home!</h3>
+			<h3 className="text-red-500">Welcome Home!</h3>
+			<Button>dsfs</Button>
 		</div>
 	);
 }
