c06fb8100046e496b300fa60db7761140413f026
MadFox <madfox@laserflare.net>
2025-07-07 03:40:28 +0400

    feat(db): explicitly pass schema & update exports

--- packages/trpc/src/auth.ts
+++ packages/trpc/src/auth.ts
@@ -1,5 +1,5 @@
+import * as schema from "@habinook/db";
 import { db } from "@habinook/db";
-import * as schema from "@habinook/db/src/schema";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { reactStartCookies } from "better-auth/react-start";
