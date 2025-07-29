e1a4def574c66a8a683192758cd43f25c171ba54
MadFox <madfox@laserflare.net>
2025-07-29 23:52:24 +0400

    feat(tss-web): add header

---
+++ apps/tanstack-web/app/components/header.tsx
@@ -0,0 +1,32 @@
+import Navbar from "@habinook/ui/components/navbar";
+import { Link } from "@tanstack/react-router";
+import { signOut, useSession } from "../lib/auth";
+
+export function Header() {
+	const session = useSession();
+
+	return (
+		<header className="px-4 md:px-6">
+			{session.data?.user ? (
+				<Navbar user={session.data.user} onSignOut={() => signOut()} />
+			) : (
+				<div className="flex h-16 items-center justify-end gap-4">
+					<Link
+						to={"/auth/$pathname"}
+						params={{ pathname: "sign-in" }}
+						className="text-sm font-medium"
+					>
+						Sign In
+					</Link>
+					<Link
+						to={"/auth/$pathname"}
+						params={{ pathname: "sign-up" }}
+						className="text-sm font-medium"
+					>
+						Get Started
+					</Link>
+				</div>
+			)}
+		</header>
+	);
+}
