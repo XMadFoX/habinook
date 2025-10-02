708591e85f4c75d89ef0fba56db20a68e4bbdc5f
MadFox <madfox@laserflare.net>
2025-10-03 02:44:11 +0400

    feat(trpc): export router inferred io types

--- packages/trpc/src/index.ts
+++ packages/trpc/src/index.ts
@@ -1,3 +1,10 @@
+import type { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+
 export { createContext } from "./context";
-export type { TrpcRouter } from "./router";
+
+import type { TrpcRouter } from "./router";
+export type { TrpcRouter };
 export { trpcRouter } from "./router";
+
+export type RouterInput = inferRouterInputs<TrpcRouter>;
+export type RouterOutput = inferRouterOutputs<TrpcRouter>;
